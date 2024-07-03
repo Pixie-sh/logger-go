adbdc8188389b9d4740ff4077bd17a5ce8a9e508
rs <rs@nullptr.pt>
2024-07-03 16:21:27 +0100

    singleton as interface; exported singleton JLogger as well

--- logger/singleton.go
+++ logger/singleton.go
@@ -8,22 +8,29 @@ import (
 )
 
 // Logger global instance to be used everywhere, until a specific instance is assigned
-var Logger, _ = NewJsonLogger(
-	context.Background(),
-	os.Stdout,
-	fmt.Sprintf("%s-%s", env.EnvAppName(), env.EnvAppVersion()),
-	env.EnvScope(),
-	fmt.Sprintf("%s-%s", env.EnvAppName(), env.EnvAppVersion()),
-	func() LogLevelEnum {
-		switch env.EnvLogLevel() {
-		case "DEBUG":
-			return DEBUG
-		case "WARN":
-			return WARN
-		case "ERROR":
-			return ERROR
-		default:
-			return LOG
-		}
-	}(),
-	[]string{TraceID})
+var Logger Interface
+var JLogger *JsonLogger
+
+func init() {
+	JLogger, _ = NewJsonLogger(
+		context.Background(),
+		os.Stdout,
+		fmt.Sprintf("%s-%s", env.EnvAppName(), env.EnvAppVersion()),
+		env.EnvScope(),
+		fmt.Sprintf("%s-%s", env.EnvAppName(), env.EnvAppVersion()),
+		func() LogLevelEnum {
+			switch env.EnvLogLevel() {
+			case "DEBUG":
+				return DEBUG
+			case "WARN":
+				return WARN
+			case "ERROR":
+				return ERROR
+			default:
+				return LOG
+			}
+		}(),
+		[]string{TraceID})
+
+	Logger = JLogger
+}
